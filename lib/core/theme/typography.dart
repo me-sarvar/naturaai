4ef4da8200e15e9c420096f8fd9f8b66203258d4
Juraev S <ibyte@Juraevs-MacBook-Pro.local>
2025-05-07 04:22:24 +0900

    feat: create folder structure, add fonts and icons, and integrate Firebase core

---
+++ lib/core/theme/typography.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class AppTypography {
+  static const TextStyle inter = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 16,
+  );
+
+  static const TextStyle interBold = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.bold,
+    fontSize: 18,
+  );
+
+  static const TextStyle lora = TextStyle(
+    fontFamily: 'Lora',
+    fontSize: 16,
+  );
+
+  static const TextStyle loraBold = TextStyle(
+    fontFamily: 'Lora',
+    fontWeight: FontWeight.bold,
+    fontSize: 20,
+  );
+
+  static const TextStyle merriweather = TextStyle(
+    fontFamily: 'Merriweather',
+    fontSize: 16,
+  );
+
+  static const TextStyle merriweatherBold = TextStyle(
+    fontFamily: 'Merriweather',
+    fontWeight: FontWeight.bold,
+    fontSize: 18,
+  );
+
+  static const TextStyle mulish = TextStyle(
+    fontFamily: 'Mulish',
+    fontSize: 16,
+  );
+
+  static const TextStyle mulishBold = TextStyle(
+    fontFamily: 'Mulish',
+    fontWeight: FontWeight.bold,
+    fontSize: 18,
+  );
+
+  static const TextStyle manrope = TextStyle(
+    fontFamily: 'Manrope',
+    fontSize: 16,
+  );
+
+  static const TextStyle manropeBold = TextStyle(
+    fontFamily: 'Manrope',
+    fontWeight: FontWeight.bold,
+    fontSize: 18,
+  );
+
+  static const TextStyle outfit = TextStyle(
+    fontFamily: 'Outfit',
+    fontSize: 16,
+  );
+
+  static const TextStyle outfitBold = TextStyle(
+    fontFamily: 'Outfit',
+    fontWeight: FontWeight.bold,
+    fontSize: 18,
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
+import 'core/firebase/firebase_options.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  runApp(const ProviderScope(child: MainApp()));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text('Natura AI!'),
-        ),
-      ),
-    );
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(debugShowCheckedModeBanner: false, title: 'Natura AI');
   }
 }
