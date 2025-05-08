abd6466fee436254284c6e4991514e8a26716609
Juraev S <ibyte@Juraevs-MacBook-Pro.local>
2025-05-09 03:24:59 +0900

    chore: add CI support for secure Firebase config using base64-encoded google-services.json

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "uz.srvr.naturaai"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "27.2.12479018"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
